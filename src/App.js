922b339c81ed1e10aa7ffa87c4a01285c52b39ca
Devraj Khanra <riku.devd@gmail.com>
2022-12-24 12:36:34 +0530

    score added

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ import Login from './components/login/Login';
 import Score from './components/score/Score';
 
 function App() {
-  const API_URL = 'http://localhost:3500/user'
+  const API_URL = 'http://192.168.0.113:3500/user'
   const navigate = useNavigate()
-
-
 
   // athenticated or not
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -19,11 +17,26 @@ function App() {
   }, [])
 
 
-
   // user credential
+  const [userId, setUserId] = useState('')
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [win, setWin] = useState(0)
+  const [lost, setLost] = useState(0)
   const [error, setError] = useState('')
+
+
+  // gameStart, player, coins & gameState
+  const [gameStart, setGameStart] = useState('')
+  const [currentPlayer, setCurrentPlayer] = useState(username)
+  const [oppositePlayer, setOppositePlayer] = useState('')
+  const [coins, setCoins] = useState(21);
+
+
+  // winner & loser
+  const [winner, setWinner] = useState('')
+  const [loser, setLoser] = useState('')
+
 
   // function to handle login submission
   const handleSubmit = async (e) => {
@@ -37,6 +50,10 @@ function App() {
       if (response.status === 200) {
         if (response.data[0].username === username && response.data[0].password === password) {
           setIsAuthenticated(true)
+          setUserId(response.data[0].id)
+          setWin(response.data[0].win)
+          setLost(response.data[0].lost)
+
           navigate('/')
         }
       } else {
@@ -47,15 +64,6 @@ function App() {
       console.log(error)
     }
   }
-
-
-
-  // gameStart, player, coins & gameState
-  const [gameState, setGameState] = useState(false)
-  const [gameStart, setGameStart] = useState('')
-  const [currentPlayer, setCurrentPlayer] = useState(username)
-  const [oppositePlayer, setOppositePlayer] = useState('')
-  const [coins, setCoins] = useState(21);
 
   // function to handle username will start the game
   const handleGameStartYes = () => {
@@ -71,61 +79,112 @@ function App() {
     setOppositePlayer(username)
   }
 
-  // winner & loser
-  const [winner, setWinner] = useState('')
-  const [loser, setLoser] = useState('')
-  const [win, setWin] = useState('')
-  const [lost, setLost] = useState('')
-
-  const handlePlayAgain = () => {
-    setCoins(21)
-    setWinner('')
-    setLoser('')
-    setGameStart('')
-    setUsername(username)
-    setOppositePlayer('AI')
-  }
-
   // function to handle coin pick
   const handleCoinPick = async (pickedCoins) => {
     await setCoins(currentCoins => currentCoins - pickedCoins)
   }
 
+  // function handle play again in the score page
+  const handlePlayAgain = () => {
+    setCoins(21)
+    setWinner('')
+    setLoser('')
+
+    setGameStart('')
+    setUsername(username)
+    setOppositePlayer('AI')
+  }
+
+  const handleLogout = () => {
+    setUserId('')
+    setUsername('')
+    setPassword('')
+    setError('')
+    setGameStart('')
+    setCurrentPlayer('')
+    setOppositePlayer('')
+    setCoins(21)
+    setWinner('')
+    setLoser('')
+    setIsAuthenticated(false)
+  }
+
+  // when coin is picked
   useEffect(() => {
+    const changePlayer = async () => {
+      // change player on coin pick
+      if (coins - 1 || coins - 2 || coins - 3 || coins - 4) {
 
-    // change player on coin pick
-    if (coins - 1 || coins - 2 || coins - 3 || coins - 4) {
+        if (currentPlayer === username) {
+          await setCurrentPlayer('AI')
+          await setOppositePlayer(username)
 
-      if (currentPlayer === username) {
-        setCurrentPlayer('AI')
-        setOppositePlayer(username)
+          // let AI play
+          // if (gameState === true) {
+          //   setTimeout(async () => {
+          //     if (coins > 0) {
+          //       await handleCoinPick(Math.floor(Math.random() * 4) + 1);
+          //     }
+          //   }, 1000);
+          // }
 
-        // let AI play
-        // if (gameState === true) {
-        //   setTimeout(async () => {
-        //     if (coins > 0) {
-        //       await handleCoinPick(Math.floor(Math.random() * 4) + 1);
-        //     }
-        //   }, 1000);
-        // }
-
-      } else {
-        setCurrentPlayer(username)
-        setOppositePlayer('AI')
+        } else {
+          await setCurrentPlayer(username)
+          await setOppositePlayer('AI')
+        }
       }
+    }
+    changePlayer()
+  }, [coins])
+
+  // when game ends
+  useEffect(() => {
+    const checkGame = async () => {
 
       // check if game ended
       if (coins < 1) {
-        setLoser(currentPlayer)
-        setWinner(oppositePlayer)
-        setTimeout(() => {
-          navigate('/score')
-        }, 500);
+        await setLoser(currentPlayer)
+        await setWinner(oppositePlayer)
+      }
+    }
+    // call function
+    checkGame()
+  }, [coins])
+
+  // after game ends score updated
+  useEffect(() => {
+    const putStatus = async () => {
+
+      if (winner === '') {
+        return
+      } else {
+        if (winner === username) {
+          await setWin(win + 1)
+        } else if (winner === 'AI') {
+          await setLost(lost + 1)
+        } else { return }
+        try {
+
+          await setTimeout(async () => {
+            const response = await axios.put(`${API_URL}/${userId}`, { id: userId, username: username, password: password, win: win, lost: lost })
+            if (response.status === 200) {
+              navigate('/score')
+            }
+          }, 1000);
+
+        } catch (error) {
+          console.log(error.message)
+        }
       }
     }
 
 
-  }, [coins])
+
+    putStatus()
+
+  }, [winner])
+
+
 
 
   return (
@@ -147,15 +206,19 @@ function App() {
         <Route
           path='/score'
           element={<Score
+            win={win}
+            lost={lost}
+            username={username}
             winner={winner}
             loser={loser}
-            handlePlayAgain={handlePlayAgain} />} />
+            handlePlayAgain={handlePlayAgain}
+            handleLogout={handleLogout} />} />
 
         {/* Login Screen */}
         <Route
           path='/login'
           element={<Login
-            username={username}
+
             setUsername={setUsername}
             password={password}
             setPassword={setPassword}
--- src/components/score/Score.js
+++ src/components/score/Score.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Score = ({ winner, loser, handlePlayAgain }) => {
+const Score = ({ win, lost, winner, loser, handlePlayAgain, handleLogout }) => {
 
     return (
         <div className='grid place-items-center pt-20'>
@@ -17,8 +17,19 @@ const Score = ({ winner, loser, handlePlayAgain }) => {
                 <p className='text-red-500 text-3xl'>{loser}</p>
             </div>
 
+            <div className='flex flex-row items-center space-x-3'>
+                <p className='font-semibold text-slate-400 text-3xl'>Win:</p>
+                <p className='text-slate-500 text-3xl'>{win}</p>
+                <p className='font-semibold text-slate-400 text-3xl'>Lost:</p>
+                <p className='text-slate-500 text-3xl'>{lost}</p>
+            </div>
+
             <div className='bg-red-500 text-red-50 p-4 hover:bg-green-500 hover:font-bold mt-4'>
                 <Link to='/' onClick={handlePlayAgain}>Play Again</Link>
+            </div>
+
+            <div className='bg-red-500 text-red-50 p-4 hover:bg-green-500 hover:font-bold mt-4'>
+                <Link to='/login' onClick={handleLogout}>Logout</Link>
             </div>
         </div >
     )
